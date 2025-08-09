94ee45c976115ca9335fd4d653e12f849bb8b347
Foysal79 <onthor312001@gmail.com>
2025-08-09 18:27:48 +0600

    page Pagination add

---
+++ src/components/Pagination.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Pagination = ({ page, setPage, canPrev, canNext })=> {
+    return (
+        <div className="flex items-center gap-2 justify-center">
+      <button
+        onClick={() => setPage((p) => Math.max(1, p - 1))}
+        disabled={!canPrev}
+        className="px-3 py-1 border rounded disabled:opacity-50"
+      >
+        Prev
+      </button>
+
+      <div className="px-3 py-1 border rounded ">Page {page}</div>
+
+      <button
+        onClick={() => setPage((currant_page) => currant_page + 1)}
+        disabled={!canNext}
+        className="px-3 py-1 border rounded disabled:opacity-50"
+      >
+        Next
+      </button>
+      
+    </div>
+    );
+};
+
+export default Pagination;
