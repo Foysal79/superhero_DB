a15a9135848e3c31d769f31b6622b69827c43fb1
Foysal79 <onthor312001@gmail.com>
2025-08-09 20:42:56 +0600

    sort button lequed glass effect add

--- src/components/SortToggle.jsx
+++ src/components/SortToggle.jsx
@@ -5,11 +5,15 @@ import { FaSortAlphaDown, FaSortAlphaUpAlt } from "react-icons/fa";
 const  SortToggle = ({ sortAsc, setSortAsc }) => {
   return (
     <button
-      onClick={() => setSortAsc((s) => !s)}
-      className="w-full px-3 py-2 gap-2 flex items-center"
-      title="Toggle sort by name"
-    >Sort {sortAsc ? <FaSortAlphaDown /> : <FaSortAlphaUpAlt />}
-    </button>
+  onClick={() => setSortAsc((p) => !p)}
+  className="w-full px-4 py-2 gap-2 flex items-center justify-center 
+             rounded-lg bg-white/10 border border-white/20 
+             backdrop-blur-md shadow-lg 
+             hover:bg-white/20 hover:scale-105 transition-all duration-300"
+  title="Toggle sort by name"
+>
+  <strong>Sort</strong> {sortAsc ? <FaSortAlphaDown /> : <FaSortAlphaUpAlt />}
+</button>
   );
 }
 
